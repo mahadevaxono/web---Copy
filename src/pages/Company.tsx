a6508c96c73b56736a9bd534876e540a88311910
mahadevaxono <mahadevaxono@gmail.com>
2026-07-06 15:21:14 +0530

    Updated Company.tsx

--- src/pages/Company.tsx
+++ src/pages/Company.tsx
@@ -25,7 +25,7 @@ export default function Company() {
       {/* Hero */}
       <div className="relative h-72 sm:h-96 overflow-hidden">
         <img
-          src="/images/webdash.png"
+          src="/images/ligh_backgroung.jpg"
           alt="Axono team"
           className="w-full h-full object-cover"
         />
